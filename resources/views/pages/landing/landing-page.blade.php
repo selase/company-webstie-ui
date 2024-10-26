cca10f1cbfb9ff068acbd2b5a6dc0ddfb56fdd56
El-Partu <laurentpartu@gmail.com>
2024-10-26 21:04:38 +0000

    completed company trustee section

--- resources/views/pages/landing/landing-page.blade.php
+++ resources/views/pages/landing/landing-page.blade.php
@@ -74,45 +74,53 @@
         </div>
     </section>
     @include('pages.landing.commnunity-talent')
-    <section class="min-h-[60vh] px-4 space-y-8">
-        <div class="flex flex-col items-center pb-10 pt-10 space-y-4">
-            <h1 class="text-[2.7rem] text-center">Over 200 companies trust Adeva</h1>
-            <p class="text-center text-xl">See how our customers are making the world a more beautiful place</p>
-        </div>
-        <div>
-            <div class="">
-                <div class=" lg:hidden relative">
-                    <span class="absolute bottom-2 left-28 text-white font-bold text-lg">Give Butter</span>
-                    <img src="{{asset('images/GiveButter.jpeg')}}" alt="give-butter" class="rounded-lg w-52">
-                </div>
-                <img src="{{asset('images/givebutter_original.svg')}}" alt="Give Butter">
-                <h1 class="">How Givebutter Partnered with Adeva To Execute Their Product Roadmap</h1>
-                <a href="#"
-                    class="flex space-x-2 hidden group-hover:flex  transform transition-transform duration-300 ease-out ease-in group-hover:translate-y-[-5px] mt-8">
-                    <p class=" text-blue-500 ">Learn More</p>
-                    <svg width="30" height="30" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                        <line x1="10" y1="50" x2="70" y2="50" stroke="blue" stroke-width="4" />
-                        <polygon points="70,30 90,50 70,70" fill="blue" />
-                    </svg>
-
-                </a>
-                <div>
-                    <p>Liran Cohen</p>
-                    <p>Co-founder and CTO, Givebutter</p>
-                </div>
-                <div>
-                    <div>
-                        <span>150%</span>
-                        <p>Increase In Engineering Velocity</p>
-                    </div>
-                    <div>
-                        <span>3 Days</span>
-                        <p>Time To Hire</p>
-                    </div>
-                </div>
+    <section class="min-h-[60vh] bg-gray-100">
+        <div class="px-4 space-y-8 py-24 lg:w-[80%] lg:mx-auto ">
+            <div class="flex flex-col items-center pb-10 pt-10 space-y-4">
+                <h1 class="text-[2.7rem] text-center ">Over 200 companies trust Adeva</h1>
+                <p class="text-center text-xl">See how our customers are making the world a more beautiful place</p>
             </div>
-            <div class="hidden">
-                <img src="{{asset('images/GiveButter.jpeg')}}" alt="give-butter">
+            <div class="flex space-x-20">
+                <div class="">
+                    <div class=" lg:hidden relative">
+                        <span class="absolute bottom-4 left-20  text-white font-bold text-2xl">Give Butter</span>
+                        <img src="{{asset('images/GiveButter.jpeg')}}" alt="give-butter" class="rounded-lg w-52">
+                    </div>
+                    <div class="space-y-8">
+                        <img src="{{asset('images/givebutter_original.svg')}}" alt="Give Butter">
+                        <h1 class="text-2xl font-bold text-[#333]/90">How Givebutter Partnered with Adeva To Execute
+                            Their
+                            Product Roadmap
+                        </h1>
+                        <a href="#" class=" flex space-x-6">
+                            <p class=" text-blue-500 text-lg ">Read full case study</p>
+                            <svg width="30" height="30" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                                <line x1="10" y1="50" x2="70" y2="50" stroke="blue" stroke-width="4" />
+                                <polygon points="70,30 90,50 70,70" fill="blue" />
+                            </svg>
+                        </a>
+                        <div class="border-l-4 border-orange-400 pl-2 bg-gray-100 lg:bg-transparent space-y-1 py-1">
+                            <p class="font-bold text-gray">Liran Cohen</p>
+                            <p class="text-sm">Co-founder and CTO, Givebutter</p>
+                        </div>
+                        <div class="flex px-2 justify-between lg:w-[80%]">
+                            <div class="space-y-1 ">
+                                <span class="text-4xl font-bold">150%</span>
+                                <p class="text-sm text-gray-500 ">Increase In Engineering Velocity</p>
+                            </div>
+                            <div class="space-y-1 ml-auto">
+                                <span class="text-4xl font-bold ">3 Days</span>
+                                <p class="text-sm text-gray-500 ">Time To Hire</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="hidden lg:block relative ">
+                    <span class="absolute bottom-10 right-6 text-white font-bold text-2xl lg:text-4xl">Give
+                        Butter</span>
+                    <img src="{{asset('images/GiveButter.jpeg')}}" alt="give-butter"
+                        class="rounded-lg h-[30vw] w-[40vw]">
+                </div>
             </div>
         </div>
     </section>
